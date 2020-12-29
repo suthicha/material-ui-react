eb3a53fd08bfedbdffcb14bee2cc1865fa64bdf5
suthicha <suthicha@outlook.com>
2020-12-29 10:02:55 +0700

    init appbar-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return <div>Hello</div>;
+  return <div>
+    <Button variant="contained">Click Me!</Button>
+  </div>;
 }
 
 export default App;
