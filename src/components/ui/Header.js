cf1c4e1090e978c21ef9a98bf548c392cd5bff89
suthicha <suthicha@outlook.com>
2021-01-09 13:09:22 +0700

    Example styling inline is done

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,33 +1,42 @@
-import React from  'react';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
-import Typography from '@material-ui/core/Typography';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
 
 function ElevationScroll(props) {
-    const { children, window } = props;
+  const { children, window } = props;
 
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-      target: window ? window() : undefined,
-    });
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
 
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
 
 export default function Header(props) {
-    return (
-        <ElevationScroll>
-            <AppBar position="fixed" color="primary">
-                <ToolBar>
-                  <Typography variant="h3">
-                    AekeLab Development
-                  </Typography>
-                </ToolBar>
-            </AppBar>
-        </ElevationScroll>
-    );
-};
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <ToolBar>
+            <Typography variant="h3">AekeLab Development</Typography>
+          </ToolBar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
+  );
+}
