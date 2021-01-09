5c0105e2285f2f30cf99f8427055514975d1a847
suthicha <suthicha@outlook.com>
2021-01-09 13:23:02 +0700

    Example appbar-logo is done

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
-import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/styles";
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -22,7 +23,11 @@ function ElevationScroll(props) {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: "3em"
   },
+  logo: {
+    height: "7em"
+  }
 }));
 
 export default function Header(props) {
@@ -31,8 +36,8 @@ export default function Header(props) {
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
-          <ToolBar>
-            <Typography variant="h3">AekeLab Development</Typography>
+          <ToolBar disableGutters>
+            <img alt="company logo" className={classes.logo} src={logo} />
           </ToolBar>
         </AppBar>
       </ElevationScroll>
