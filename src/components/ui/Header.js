3fea1b208f90b3fd05b1ebb645ef90041b9cf7df
suthicha <suthicha@outlook.com>
2021-01-09 14:50:55 +0700

    Example react router done

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
+import { Link } from 'react-router-dom';
 
 import logo from "../../assets/logo.svg";
 
@@ -31,6 +32,12 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "7em",
   },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    }
+  },
   tabContainer: {
     marginLeft: 'auto'
   },
@@ -45,34 +52,76 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '50px',
     marginRight: '25px',
     height: '45px',
-  }
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
-
   const [value, setValue ] = useState(0);
 
   const handleChange = (e, value) => {
     setValue(value);
-  }
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
 
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <ToolBar disableGutters>
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}>
             <img alt="company logo" className={classes.logo} src={logo} />
+            </Button>
             <Tabs
               value={value}
               className={classes.tabContainer}
               onChange={handleChange}
               indicatorColor="primary">
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to=""
+                label="Home" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us" />
             </Tabs>
             <Button variant="contained" color="secondary" className={classes.button}>
               Free Estimate
