243eb170d8fd2e80a80d74d8746b5d7df7d97e40
suthicha <suthicha@outlook.com>
2021-01-17 18:16:37 +0700

    footer setup

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./ui/Theme";
 import Header from "../components/ui/Header";
+import Footer from "../components/ui/Footer";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/" component={() => <div style={{height: "600px"}}>Home</div>} />
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
           <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/contact" component={() => <div>Contact Us</div>} />
           <Route exact path="/estimate" component={() => <div>Estimate</div>} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ src/components/ui/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import footerAdornment from "../../assets/Footer Adornment.svg";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.blue,
+    width: "100%",
+    zIndex: 9999,
+    position: "relative",
+  },
+  adornment: {
+      width: "25em",
+      verticalAlign: "bottom",
+      [theme.breakpoints.down("md")]: {
+          width: "21em"
+      },
+      [theme.breakpoints.down("xs")]: {
+        width: "15em"
+    },
+  }
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <img
+        src={footerAdornment}
+        alt="black decorative slash"
+        className={classes.adornment}
+        />
+    </footer>
+  );
+}
